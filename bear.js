d99afa0cf8fece7d8fea416d8d981d2b66b3c777
raphortegacode <raphaelmar.ortega@gmail.com>
2023-01-27 09:39:42 +0800

    final

--- bear.js
+++ bear.js
@@ -1,15 +1,21 @@
 function myFunction(x) {
-    // var y = document.getElementById("nav-collapse");
+    var y = document.getElementById("nav-collapse");
     var btn = document.getElementById("navbtn");
+    var fb = document.getElementById("fb");
+    var twitter = document.getElementById("twitter");
+    var yt = document.getElementById("youtube");
 
     if (x.matches) { // If media query matches
-        // y.style.display = 'none';
+        y.style.display = 'none';
         btn.style.display = 'block';
 
     } else {
 
         btn.style.display = 'none';
-        // y.style.display = 'block';
+        y.style.display = 'block';
+        fb.innerHTML = "";
+        twitter.innerHTML = "";
+        yt.innerHTML = "";
     }
 }
 
@@ -17,16 +23,28 @@ function myFunction(x) {
 navbtn.addEventListener('click', (e) => {
     var y = document.getElementById("nav-collapse");
     console.log(event.detail)
-
+    var fb = document.getElementById("fb");
+    var twitter = document.getElementById("twitter");
+    var yt = document.getElementById("youtube");
     if (y.style.display === "none") {
         y.style.display = "block";
+        fb.innerHTML = " | Facebook";
+        twitter.innerHTML = " | Twitter";
+        yt.innerHTML = " | Youtube";
     } else {
         y.style.display = "none";
+        fb.innerHTML = "";
+        twitter.innerHTML = "";
+        yt.innerHTML = "";
     }
 })
 
+function submitForm() {
+    document.getElementById("contactForm").submit();
+}
 
-var x = window.matchMedia("(max-width: 450px)");
+
+var x = window.matchMedia("(max-width: 600px)");
 
 myFunction(x);
 
